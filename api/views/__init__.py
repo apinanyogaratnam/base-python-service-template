e5fc9fbfcf2b226e1494d3513c293012068d74bd
apinanyogaratnam <apinanapinan@icloud.com>
2022-09-24 21:49:55 -0400

    feat: created create_app function

---
+++ api/views/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Api
+
+from api.controllers.health_check import HealthCheck
+from flasgger import Swagger
+
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+    swagger = Swagger(app)
+
+    app.config['SWAGGER'] = {
+        'title': 'WeMeta Success Metrics Service',
+    }
+
+    # Initialize Config
+    app.config.from_pyfile('../../config.py')
+
+    # Register Resources
+    api.add_resource(HealthCheck, '/healthcheck')
+
+    return app
